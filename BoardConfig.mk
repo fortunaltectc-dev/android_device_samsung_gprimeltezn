19ef0f9857c12b265acefbc4eb39e1d3f14f59a8
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-08 12:32:49 -0400

    gprimelte: Don't build keymaster module
     * The keystore hal breaks booting on the device.
    
    Change-Id: I78df4421272c7dc61e9f9a3fa5672375e73dc676

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ TARGET_OTA_ASSERT_DEVICE := gprimeltectc,fortunaltectc
 # Kernel
 TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_fortuna_ctc_defconfig
 
+# Keymaster
+TARGET_PROVIDES_KEYMASTER := true
+
 # Partition sizes
 BOARD_SYSTEMIMAGE_PARTITION_SIZE    :=  1468006400
 BOARD_USERDATAIMAGE_PARTITION_SIZE  := 5834256384
